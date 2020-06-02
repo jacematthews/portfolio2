252b5cdfc225275a903ce59abe6b2410d7904f50
Jupiter3609 <63023381+Jupiter3609@users.noreply.github.com>
2020-06-02 15:17:47 -0400

    Initial Commit
    Portfolio Initial Commit to master

---
+++ Portfolio Jace/Portfolio Jace/js/SunsetHills.js
@@ -0,0 +1,15 @@
+﻿function SunsetHills(buildings) {
+    
+    let tallest = Number.MIN_VALUE;
+
+    let sunsetViews = new Array();
+
+    for (var index = 0; index < buildings.length; index++) {
+        if (buildings[index] > tallest) {
+            tallest = buildings[index];
+            sunsetViews.push(`The Building in position ${index} with height ${tallest} can see the sunset. <br>`);
+        }
+    }
+
+    return sunsetViews.join("");
+}
---
+++ Portfolio Jace/Portfolio Jace/js/scripts.js
@@ -0,0 +1,22 @@
+﻿$("#btnFizzBuzz").on("click", function () {
+    var fizzNum = Number($("#fizzNum").val());
+    var buzzNum = Number($("#buzzNum").val());
+    var arrOutput = new Array();
+    for (var loop = 1; loop <= 100; loop++) {
+        if ((loop % fizzNum == 0) && (loop % buzzNum == 0)) {
+            arrOutput.push("<span class='fizzbuzz'>FIZZ-BUZZ</span>");
+        }
+        else if (loop % fizzNum == 0) {
+            arrOutput.push("<span class='fizz'>FIZZ</span>");
+        }
+        else if (loop % buzzNum == 0) {
+            arrOutput.push("<span class='buzz'>BUZZ</span>");
+        }
+        else {
+            arrOutput.push(loop);
+        }
+    }
+
+    $("#output").html(arrOutput.join(", "));
+
+});
